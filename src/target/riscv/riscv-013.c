e7aea57e1eca6455ae664f212b944e75d38d8fc2
Micha Hoiting <micha.hoiting@gmail.com>
2020-12-22 10:31:34 +0100

    Fixed compilation issues.

--- src/target/riscv/riscv-013.c
+++ src/target/riscv/riscv-013.c
@@ -2545,7 +2545,7 @@ static int write_memory_bus_v0(struct target *target, target_addr_t address,
 		access = 0;
 		access = set_field(access, DMI_SBCS_SBACCESS, size/2);
 		dmi_write(target, DMI_SBCS, access);
-		LOG_DEBUG("\r\naccess:  0x%08" PRIx64, access);
+		LOG_DEBUG("\r\naccess:  0x%08" PRIx64, (long unsigned int) access);
 		LOG_DEBUG("\r\nwrite_memory:SAB: ONE OFF: value 0x%08" PRIx64, value);
 		dmi_write(target, DMI_SBDATA0, value);
 		return ERROR_OK;
@@ -2556,7 +2556,7 @@ static int write_memory_bus_v0(struct target *target, target_addr_t address,
 	access = 0;
 	access = set_field(access, DMI_SBCS_SBACCESS, size/2);
 	access = set_field(access, DMI_SBCS_SBAUTOINCREMENT, 1);
-	LOG_DEBUG("\r\naccess:  0x%08" PRIx64, access);
+	LOG_DEBUG("\r\naccess:  0x%08" PRIx64, (long unsigned int) access);
 	dmi_write(target, DMI_SBCS, access);
 
 	/*2)set the value according to the size required and write*/
@@ -3244,9 +3244,8 @@ static int riscv013_test_sba_config_reg(struct target *target,
 	}
 
 	uint32_t max_sbdata_regs = get_num_sbdata_regs(target);
-	uint32_t num_sbdata_regs;
-
-	uint32_t rd_buf[num_sbdata_regs];
+	uint32_t num_sbdata_regs = 0;
+	uint32_t rd_buf[num_sbdata_regs+1];
 
 	/* Test 1: Simple write/read test */
 	test_passed = true;
